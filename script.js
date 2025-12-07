716db5bbd5b39b9d35efeb533f51b45f43c1a77e
Phallaboth <beflexter@gmail.com>
2025-12-07 21:00:11 +0700

    updated

--- script.js
+++ script.js
@@ -6,11 +6,35 @@ $('.owl-carousel').owlCarousel({
         0:{
             items:1
         },
-        600:{
+        50:{
             items:3
         },
-        1000:{
+        100:{
             items:5
         }
     }
 })
+
+const data = [
+    {
+        id:1,
+        title:"Learn HTML",
+        teacher:"John Doe",
+        image:"",
+        discription:"Learn the basics of HTML and build your first web page.",
+    },
+    {
+        id:1,
+        title:"Learn CSS",
+        teacher:"Jason Williams",
+        image:"",
+        discription:"CSS is the art of turning structure into style—where code meets creativity."
+    },
+    {
+        id:3,
+        title:"Learn JavaScript",
+        teacher:"Rose Simmons",
+        image:"",
+        discription:"JavaScript is the programming language of the Web. It makes web pages interactive and dynamic.",
+    },
+];
